8ae49afe28d9c354e29f364d555bb5fdd9f9adb3
bsheep <blacksheep.feather@gmail.com>
2011-03-09 22:54:15 +0900

    pluginize

--- sidekick.js
+++ sidekick.js
@@ -1,21 +1,23 @@
 (function($) {
- $(document).ready(function() {
-     $('#sidekick').bind('click', function() {
+ $.fn.sidekicker = function(option) {
+     $(this).live('click', function() {
          var kicked = $(this);
-         $('#container').animate({
+         var kicker = (option && option.kicker) ? $(option.kicker) : $('#kicker');
+         var container = (option && option.container) ? $(option.container) : $('#container');
+         console.log(container);
+         container.animate({
              left: '-850px'
          }, 1000, function() {
              var kicker_left = kicked.offset().left + 310;
              var kicker_top = $('html, body').scrollTop() + 10;
-             $('#kicker').appendTo('body').css({left: kicker_left, top: kicker_top}).show('slow');
+             kicker.appendTo('body').css({left: kicker_left, top: kicker_top}).show('slow');
+         });
+         $('#kickback').live('click', function() {
+             kicker.hide('normal');
+             container.animate({left:'0px'});
+             return false;
          });
          return false;
      });
-
-     });
-     $('#kickback').live('click', function() {
-         $('#kicker').hide('normal');
-         $('#container').animate({left:'0px'});
-         return false;
-     });
- })(jQuery);
+ };
+})(jQuery);
